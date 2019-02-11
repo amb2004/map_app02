ab9ee7079acae5b965fc775ade1a65e2bd8a9045
unknown <bennaoui.ameur@gmail.com>
2019-02-11 06:01:23 +0100

    version 02

---
+++ install_package.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install imagemagick6.9
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
+sudo pip install flask
+sudo pip install pillow
+sudo pip install matplotlib
+sudo pip install imutils
+sudo pip install opencv-python
+sudo apt-get install tesseract-ocr
+sudo apt-get install tesseract-ocr-eng
+sudo python3 -m pip install opencv-contrib-python
+sudo pip install Wand
+sudo pip install scipy
+sudo pip install pytesseract
